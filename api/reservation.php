d75c3efb920133dd9db89d5a1f1f99ce2a629af2
Arinouchkine <arinouchkine@gmail.com>
2018-09-19 10:41:05 +0200

    API reservation

---
+++ api/reservation.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arino
+ * Date: 19/09/2018
+ * Time: 09:54
+ */
+
+include 'config.php';
+
+if(isset($_POST['user'])&&isset($_POST['offre'])&&isset($_POST['dateD'])&&isset($_POST['dateF']))
+{
+    $datenow = date('Ymd');
+    $sql="INSERT INTO `reservation`( `id_offre`, `id_use`, `date_res`, `date_deb`, `date_fin`) VALUES (".$_POST['offre'].",".$_POST['user'].",".$datenow.",".$_POST['dateD'].",".$_POST['dateF'].")";
+    $query	= $db->prepare($sql);
+
+    try
+    {
+        $query->execute();
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: POST, GET");
+        header("Location: ../appartements.php");
+    }
+    catch (PDOException $e)
+    {
+        echo $e->getMessage();
+        exit;
+    }
+}
